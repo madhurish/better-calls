46169f6ebe96ea912720f3dee735679dea1784d1
abhishek <abhishekpoonia22@gmail.com>
2019-03-10 09:26:52 +0530

    Add dashboard component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {LoginComponent} from './login/login.component';
 import {SignupComponent} from './signup/signup.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 const appRoutes: Routes = [
   {path: 'home', component: HomeComponent},
@@ -23,7 +24,8 @@ const appRoutes: Routes = [
     HomeComponent,
     LoginComponent,
     SignupComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
